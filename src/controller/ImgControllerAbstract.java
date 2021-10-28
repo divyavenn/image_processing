c79dfe9ec08cfc2dd15cc1ee0ba7a627d2a07025
divyavenkatraman <44758649+divyavenkatraman@users.noreply.github.com>
2021-10-28 17:32:06 -0400

    update image controller

--- src/controller/ImgControllerAbstract.java
+++ src/controller/ImgControllerAbstract.java
@@ -8,6 +8,51 @@ public abstract class ImgControllerAbstract implements ImgController{
   ImgView view;
   Readable in;
 
+  final String load = "load";
+  final String brighten = "brighten";
+  final String save = "save";
+  final String rc = "just-red";
+  final String gc = "just-green";
+  final String bc = "just-blue";
+  final String vc = "just-value";
+  final String lc = "just-luma";
+  final String ic = "just-intensity";
+  final String hflip = "hflip";
+  final String vflip = "hflip";
+
+
+  /**
+   * Returns true if command requires an image path as argument.
+   * Assumes that input is a valid command.
+   * @param command the command
+   * @return true or false.
+   */
+  public boolean pathArg(String command){
+    if (command.equals(load)) {
+      return true;
+    }
+    else if(command.equals(save)) {
+      return true;
+    }
+    else return false;
+  }
+
+
+  /**
+   * Returns true if command requires an increment as an argument.
+   * Assumes that input is a valid command.
+   * @param command the command
+   * @return true or false.
+   */
+  public boolean incrementArg(String command){
+    if (command.equals(brighten)) {
+      return true;
+    }
+    else return false;
+  }
+
+
+
 
   /**
    * Constructs an ImgController object.
