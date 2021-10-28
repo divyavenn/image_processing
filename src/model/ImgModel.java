ae9d964478f9d4cd034e269af5352050ca199a73
SymNC <93349991+SymNC@users.noreply.github.com>
2021-10-28 17:39:11 -0400

    ImgModel interface method signatures

--- src/model/ImgModel.java
+++ src/model/ImgModel.java
@@ -1,4 +1,32 @@
 package model;
 
+/**
+ *
+ */
 public interface ImgModel {
+
+
+  /**
+   * Flip an image horizontally.
+   */
+  void horizontalFlip(String imgName);
+
+  /**
+   * Flip an image vertically
+   */
+  void verticalFlip(String imgName);
+
+  /**
+   *
+   */
+  void brighten(String imgName);
+
+  /**
+   *
+   */
+  void grayscaleR(String imgName);
+
+
+
+
 }
