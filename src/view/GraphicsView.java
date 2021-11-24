9e3bbfef53b964960086b377fbfb8a7d72bfb6bf
divyavenkatraman <44758649+divyavenkatraman@users.noreply.github.com>
2021-11-24 12:28:36 -0500

    we have liftoff

--- src/view/GraphicsView.java
+++ src/view/GraphicsView.java
@@ -24,12 +24,32 @@ import util.Tools;
 
 public class GraphicsView extends JFrame implements IGraphicsView {
   protected ImgModel model;
-  Histogram hist;
   ArrayList<JMenuItem> commandButtons;
   private Img currentImg;
   private JLabel imageWindow;
+  private HistogramPanel histogramPanel;
   private JMenuBar MenuBar;
   private JMenu Menu;
+
+  private void buildHistogramWindow(){
+    histogramPanel = new HistogramPanel();
+    this.add(histogramPanel, BorderLayout.WEST);
+  }
+
+  private void buildImageWindow() {
+    // build separate method: setPicture, setPicture is called after every command
+    // this method should just build the WINDOW
+    imageWindow = new JLabel();
+    imageWindow.setMinimumSize(new Dimension(500, 500));
+    imageWindow.setBorder(new LineBorder(Color.black));
+    imageWindow.setOpaque(true);
+    imageWindow.setBackground(Color.gray);
+    JScrollPane scrollPane = new JScrollPane(imageWindow);
+    scrollPane.setMinimumSize(new Dimension(this.getWidth() - histogramPanel.getWidth(), 500));
+    scrollPane.setPreferredSize(new Dimension(500, 500));
+    this.add(scrollPane, BorderLayout.CENTER);
+  }
+
 
   /**
    * Creates a ImgView object.
@@ -49,39 +69,78 @@ public class GraphicsView extends JFrame implements IGraphicsView {
     this.setLayout(new BorderLayout());
     initImage();
     buildCommandButtons();
+    buildHistogramWindow();
     buildImageWindow();
-    buildHist();
-    pack();
-  }
-
-  // setPicture(
-
-  private void buildHist() {
-    hist = new Histogram(currentImg);
-    this.add(hist, BorderLayout.PAGE_END);
-    hist.setLayout(new BorderLayout());
-    hist.setSize(new Dimension(100, 100));
   }
 
 
-  private void buildImageWindow() {
-    // build separate method: setPicture, setPicture is called after every command
-    // this method should just build the WINDOW
-    imageWindow = new JLabel();
-    imageWindow.setMinimumSize(new Dimension(4, 2));
-    imageWindow.setBorder(new LineBorder(Color.black));
-    imageWindow.setOpaque(true);
-    imageWindow.setBackground(Color.gray);
-    JScrollPane scrollPane = new JScrollPane(imageWindow);
-    scrollPane.setPreferredSize(new Dimension(500, 500));
-    this.add(scrollPane, BorderLayout.CENTER);
+
+  private void buildHistogram() {
+    histogramPanel.setImage(currentImg);
   }
+
 
   private void buildImage() {
     BufferedImage bImg = Tools.getBuffImg(currentImg);
     ImageIcon image = new ImageIcon(bImg);
     imageWindow.setIcon(image);
   }
+
+
+  public void doCommand(String s) throws IOException {
+    Command c = Command.getCommand(s);
+    if (c == null) throw new IllegalArgumentException("Not a valid command");
+    Map<Parameter, String> paramValues = new HashMap();
+    for (Parameter p : Parameter.values()) {
+      paramValues.put(p, null);
+    }
+    for (Parameter p: Parameter.values()) {
+      if (Command.needsParam(c.toString(),p)) {
+        String input = getInput(p);
+        paramValues.put(p, input);
+      }
+    }
+    c.run(model, paramValues);
+    textBox(c.acknowledge(paramValues));
+    currentImg = model.getImage(paramValues.get(Parameter.destinationImage));
+    System.out.println(paramValues.get(Parameter.destinationImage));
+    System.out.println(currentImg.toString());
+    buildImage();
+    buildHistogram();
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
   private void buildCommandButtons() {
@@ -166,30 +225,6 @@ public class GraphicsView extends JFrame implements IGraphicsView {
   }
 
 
-  public void doCommand(String s) throws IOException {
-    Command c = Command.getCommand(s);
-    if (c == null) throw new IllegalArgumentException("Not a valid command");
-    Map<Parameter, String> paramValues = new HashMap();
-    for (Parameter p : Parameter.values()) {
-      paramValues.put(p, null);
-    }
-    for (Parameter p: Parameter.values()) {
-      if (Command.needsParam(c.toString(),p)) {
-        String input = getInput(p);
-        paramValues.put(p, input);
-      }
-    }
-    c.run(model, paramValues);
-    textBox(c.acknowledge(paramValues));
-    currentImg = model.getImage(paramValues.get(Parameter.destinationImage));
-    System.out.println(paramValues.get(Parameter.destinationImage));
-    System.out.println(currentImg.toString());
-    buildImage();
-    requestFocus();
-    revalidate();
-    repaint();
-  }
-
   public void textBox(String text) {
     JOptionPane.showMessageDialog(null, text, "Message", JOptionPane.ERROR_MESSAGE);
   }
@@ -251,10 +286,8 @@ public class GraphicsView extends JFrame implements IGraphicsView {
     return "";
   }
 
-  public void refresh() {
-    this.requestFocus();
-    this.repaint();
-  }
+
+
 
 
 }
---
+++ src/view/HistogramPanel.java
@@ -0,0 +1,158 @@
+package view;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+import img.Img;
+import img.Pixel;
+
+class HistogramPanel extends JPanel {
+  private int[] redCoords;
+  private int[] blueCoords;
+  private int[] greenCoords;
+  private int[] intensityCoords;
+
+  int max;
+
+
+  private final int graphHeight = 500;
+  private final int graphWidth = 300;
+
+  private double unitX = 1;
+  private double unitY = 1;
+
+  private int startX = 10;
+  private int startY = 0;
+
+  private int endX = startX + 300;
+  private int endY = startY + 500;
+
+  private int prevX = startX;
+  private int prevY = startY;
+
+
+  boolean hasStarted;
+
+  public HistogramPanel() {
+     redCoords = new int[]{1,2,3,4};
+     blueCoords = new int[]{2,5,6,8};
+     greenCoords = new int[]{3,15,8,9};
+     intensityCoords = new int[]{2,13,7,10};
+    this.setBackground(Color.LIGHT_GRAY);
+    hasStarted = false;
+    this.setMinimumSize(new Dimension(endX, endY));
+    max = endY;
+  }
+
+
+  public int getWidth(){
+    return endX + 10;
+  }
+  public void setImage(Img img){
+    Pixel pixel;
+    int height = img.getHeight();
+    int width = img.getWidth();
+    redCoords = new int[256];
+    blueCoords = new int[256];
+    greenCoords = new int[256];
+    intensityCoords = new int[256];
+    for (int y = 0; y < height; y++) {
+      for (int x = 0; x < width; x++) {
+        pixel = img.getPixel(y, x);
+        int r = pixel.getRed();
+        int g = pixel.getGreen();
+        int b = pixel.getBlue();
+        int intensity = pixel.getIntensity();
+        redCoords[r] = redCoords[r] + 1;
+        blueCoords[b] = blueCoords[b] + 1;
+        greenCoords[g] = greenCoords[g] + 1;
+        intensityCoords[intensity] = intensityCoords[intensity] + 1;
+        max = Math.max(intensityCoords[intensity],
+                Math.max(greenCoords[g],
+                        Math.max(blueCoords[b],
+                                Math.max(max,redCoords[r]))));
+      }
+    }
+    System.out.println("MAX:" + max);
+    hasStarted = true;
+    repaint();
+  }
+
+  @Override
+  public void paint(Graphics g) {
+    setUnits();
+    super.paint(g);
+    System.out.println(redCoords.length);
+    Graphics2D g2d = (Graphics2D) g;
+
+
+    if (hasStarted) {
+      g2d.setColor(Color.RED);
+      g2d.fill(new Rectangle(100, 100));
+    }
+
+    //We draw in the following 2 loops the grid so it's visible what I explained before about each "unit"
+    //We draw the axis here instead of before because otherwise they would become blue colored.
+    g2d.setColor(Color.BLACK);
+    g2d.drawLine(startX, startY, startX, endY);
+    g2d.drawLine(startX, endY, endX, endY);
+
+    System.out.println("RED LINE");
+    //We draw each of our coords in red color
+    g2d.setColor(Color.RED);
+    for (int r : redCoords) {
+      System.out.println(r);
+      g2d.drawLine(prevX, prevY, prevX += unitX, prevY = endY-(int)(Math.round(r*unitY)));
+      System.out.println(prevY);
+    }
+
+    resetBaseCoords();
+    System.out.println("BLUE LINE");
+    g2d.setColor(Color.BLUE);
+    for (int b : blueCoords) {
+      System.out.println(b);
+      System.out.println(prevY);
+      g2d.drawLine(prevX, prevY, prevX += unitX, prevY = endY-(int)(Math.round(b * unitY)));
+    }
+
+    resetBaseCoords();
+    System.out.println("GREEN LINE");
+
+    g2d.setColor(Color.GREEN);
+    for (int gr : greenCoords) {
+      System.out.println(gr);
+      System.out.println(prevY);
+      g2d.drawLine(prevX, prevY, prevX += unitX, prevY = endY-(int)(Math.round(gr * unitY)));
+    }
+
+    resetBaseCoords();
+
+    System.out.println("INTENSITY LINE");
+    g2d.setColor(Color.DARK_GRAY);
+    for (int intensity : intensityCoords) {
+      System.out.println(intensity);
+      System.out.println(prevY);
+      g2d.drawLine(prevX, prevY, prevX += unitX, prevY = endY-(int)(Math.round(intensity * unitY)));
+    }
+    resetBaseCoords();
+  }
+
+
+  private void resetBaseCoords(){
+    prevX = startX;
+    prevY = 0;
+  }
+
+
+  private void setUnits(){
+    unitX = (double)graphWidth/(double)redCoords.length;
+    System.out.println("Set unit X: " + unitX);
+    unitY = (double)graphHeight/(double)max;
+    System.out.println("Set unit y: " + unitY);
+  }
+  @Override
+  public Dimension getPreferredSize() {
+    return new Dimension(getWidth(), endY + 100);
+  }
+}
